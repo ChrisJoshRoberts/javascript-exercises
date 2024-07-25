bdd4f801465a7cf2a4ce819918cecfdd7b355558
Chris Roberts <chrisrobertscollective@gmail.com>
2024-07-25 14:32:01 +0100

    completed revere string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+  
+  const reversedString = string.split('').reverse().join('');
+  return reversedString
 };
 
 // Do not edit below this line
