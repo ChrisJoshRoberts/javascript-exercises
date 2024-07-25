6d4732205508f96e200ae9f2b62395a54b4e6c99
Chris Roberts <chrisrobertscollective@gmail.com>
2024-07-25 14:06:06 +0100

    passing test 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+  let string = '';
+  for(let i = 0; i < times; i++) {
+    string += word;
+  }
+  console.log(string);
+  return string;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
